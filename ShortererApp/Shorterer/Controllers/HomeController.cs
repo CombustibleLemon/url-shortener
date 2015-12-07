15f5a6f59c457303f7780234b9adbd5c5383ab7b
Daniel Leonard <daniel@danleonard.us>
2015-12-06 19:04:53 -0600

    More cleaning

--- ShortererApp/Shorterer/Controllers/HomeController.cs
+++ ShortererApp/Shorterer/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace Shorterer.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
